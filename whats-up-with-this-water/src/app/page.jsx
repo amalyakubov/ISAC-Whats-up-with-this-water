ed24e8c71efec3f1e7fa69c71a66aa704cb2519b
Amal Yakubov <amal.yakubov7777@gmail.com>
2023-10-07 20:18:41 +0500

    Set up initial webpage based on nextJS framework

---
+++ whats-up-with-this-water/src/app/page.jsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        height: "100vh",
+      }}
+    >
+      <div
+        style={{
+          color: "white",
+          height: "50vh",
+          width: "100%",
+          border: "4px solid black",
+          backgroundColor: "#010048  ",
+        }}
+      >
+        <div></div>
+      </div>
+    </main>
+  );
+}
